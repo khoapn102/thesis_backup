64943ea7ca57fd561571c7430b16094179eff5e2
khoapn102 <nguyenphan2349@gmail.com>
2017-02-20 02:10:52 +0700

    Seventh

--- iuopensys_course/models/student_registration.py
+++ iuopensys_course/models/student_registration.py
@@ -1,4 +1,5 @@
 from openerp import models, fields, api
+from datetime import datetime
 from openerp.exceptions import ValidationError
 
 class StudentRegistration(models.Model):
@@ -26,26 +27,59 @@ class StudentRegistration(models.Model):
     
     ext_note = fields.Text('Note')
     
+#     @api.multi
+#     def edit_form_view(self):
+#         self.is_edit_mode = True
+#         return {
+#                 'name': "Registration Form",
+#                 'type': 'ir.actions.act_window',
+#                 'view_mode': 'form',
+#                 'view_type': 'form',
+#                 'res_model': 'student.registration',
+#                 'res_id': self.id,                
+#                 'target': 'inline',
+#                 'context': self._context,
+#                 }
+    
     @api.constrains('offer_course_ids')
     def _validate_registered_course(self):
         for record in self:
             if record.offer_course_ids:
                 sum = 0
                 for course in record.offer_course_ids:
+                    # Check if course added is unvail
+                    if course.avail_students == 0:
+                        raise ValidationError('Some courses are not available (colored RED). Please choose different course.')
+                    # Check for max credits
                     sum += course.course_id.number_credits
                     if sum > record.crs_reg_id.max_credits:
                         temp = record.crs_reg_id.max_credits
                         raise ValidationError('Cannot register for more than ' + str(temp))
+                    
     @api.model
     def create(self, vals):
         vals['is_created'] = True
         curr_reg = super(StudentRegistration,self).create(vals)        
         return curr_reg
     
-#     @api.multi
-#     def write(self, vals):
-#         """
-#         1. Check Regsitration time
-#         2. 
-#         """
+    @api.multi
+    def write(self, vals):
+        """
+        1. Check Regsitration time
+        2. 
+        """
+        for record in self:
+            now = datetime.now()
+            start = datetime.strptime(record.start_datetime, '%Y-%m-%d %H:%M:%S')
+            end = datetime.strptime(record.end_datetime,'%Y-%m-%d %H:%M:%S')
+            if not start <= now <= end:
+                raise ValidationError('Out of Registration Period. Please try again later !')
+            else:
+                if 'offer_course_ids' in vals:
+                    sum_cred = 0
+                    for course in record.offer_course_ids:
+                        sum_cred += course.number_credits
+                        
+                    
+        return super(StudentRegistration,self).write(vals)
         
